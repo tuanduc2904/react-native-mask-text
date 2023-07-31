e3998362dacf0b8aee6e1bb155c5eb5789be30ec
nguyenduc <30556451+tuanduc2904@users.noreply.github.com>
2023-07-31 14:30:50 +0700

    Update mask.ts

--- src/utils/mask.ts
+++ src/utils/mask.ts
@@ -15,7 +15,7 @@ function unMask(value: string, type: 'custom' | 'currency' = 'custom') {
     if (!value) return '0'
 
     const unMaskedValue = value.replace(/\D/g, '')
-    const number = parseInt(unMaskedValue.trimStart())
+    const number = parseInt(unMaskedValue)
 
     return number.toString()
   }
